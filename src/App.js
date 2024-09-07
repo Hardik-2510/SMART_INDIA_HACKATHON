e50571c4c7408dbdb34d7fecb125c208e8bfac0e
kevindesai1312 <144267466+kevindesai1312@users.noreply.github.com>
2024-09-07 17:51:41 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Navbar from './componetes/navbar/navbar';
+
+
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componetes/navbar/navbar.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+import './navbar.css'
+import logo from '../assist/logo1.jpg'
+import cart_icon from '../assist/shopping-cart.png'
+
+const Navbar = () => {
+    const [menu,setmenu] = useState("shop")
+  return (
+    <div className='navbar'>
+      <div className='nav-logo'>
+         <img src={logo} alt=''></img>
+         <p>FARMKILT</p>
+        </div> 
+        <ul className='nav-menu'>
+            <li onClick={()=>{setmenu('shop')}}>shop{menu=="shop"?<hr/>:<></>}</li>
+            <li onClick={()=>{setmenu('vegetables')}}>vegetable{menu=="vegetables"?<hr/>:<></>}</li>
+            <li onClick={()=>{setmenu('fruites')}}>fruites{menu=="fruites"?<hr/>:<></>}</li>
+            <li onClick={()=>{setmenu('meat/fish')}}>meat/fish{menu=="meat/fish"?<hr/>:<></>}</li>
+        </ul>
+        <div className='nav-login-cart'>
+            <button> login </button>
+            <img src={cart_icon} alt=''></img>
+            <div className='nav-cart-count'>0</div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
