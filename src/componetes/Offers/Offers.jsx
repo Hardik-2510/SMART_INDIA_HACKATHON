856f5bb338a80b618b3d7f71d390e9e7e0208ed1
Hardik Patel <125751962+Hardik-2510@users.noreply.github.com>
2024-09-07 22:05:46 +0530

    assets added

---
+++ src/componetes/Offers/Offers.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Offers.css'
+
+const Offers = () => {
+  return (
+    <div className='offers'>
+        <div className='offers-left'>
+            <h1>Daily Sale</h1>
+            <h1>Offers For Your Health</h1>
+            <p>ONLY ON ORGANIC PRODUCTS</p>
+            <button>Check Now</button>
+        </div>
+        <div className='offers-right'>
+            
+        </div>      
+    </div>
+  )
+}
+
+export default Offers
--- src/pages/shop.jsx
+++ src/pages/shop.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Offers from '../componetes/Offers/Offers'
 
 const shop = () => {
   return (
     <div>
-      
+      <Offers/>
     </div>
   )
 }
